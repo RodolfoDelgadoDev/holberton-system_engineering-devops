bf706781d69468c61323a28ea7e24477392af99a
Ro8s <rodrigo.delgado3132@gmail.com>
2021-07-05 23:59:15 +0000

    hm

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(\W*\w+)......(\W*\w+).........(\W*\w*\W*\w*\W*\w*\W*\w*\W*\W*\w*)/).join
+puts ARGV[0].scan(/from:(\W*\w+)......(\W*\w+).........(\W*\w*\W*\w*\W*\w*\W*\w*\W*\W*\w*)/).join(separator = ",")

3fd534b6b0c68d074608841ae996a52152c63540
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-13 15:19:17 +0000

    fix tkm javi

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
-# create a manifest that kills a process named killmenow.
+# kills a procces called killmenow
 
 exec {'pkill':
-  path => ['/usr/bin', 'bin/pkill'],
-  command => 'pkill -9 killmenow',
+  path     => ['usr/bin', 'bin/pkill'],
+  command  => 'pkill -9 killmenow',
   provider => 'shell',
 }

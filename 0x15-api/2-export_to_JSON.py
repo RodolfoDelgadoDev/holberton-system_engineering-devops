463e85d280fdebb53b8a3aa124a6a5b2278d0dda
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-13 23:14:39 +0000

    added ex 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Python script to export data in the CSV format """
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+        user = requests.get('https://jsonplaceholder.typicode.com/users?id={}'
+                            .format(sys.argv[1]))
+        t = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1]))
+        user_data = user.json()
+        tasks_data = t.json()
+        filename = '{}.json'.format(sys.argv[1])
+        uid = "{}".format(sys.argv[1])
+        lista = []
+        for tasks in tasks_data:
+                task = '{}'.format(tasks['title'])
+                username = '{}'.format(user_data[0]['username'])
+                com = '{}'.format(tasks['completed'])
+                di = {'task': task, 'completed': com, 'username': username}
+                lista.append(di)
+        dic = {uid: lista}
+        with open(filename, 'w+', encoding='utf-8') as f:
+                json.dump(dic, f)

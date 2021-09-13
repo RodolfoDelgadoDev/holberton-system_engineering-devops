a11814b0225b3873fdab4797bea44396d284bab6
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-13 19:25:11 +0000

    added ex 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""given employee ID, returns information about his/her TODO list progress """
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users?id={}'
+                        .format(sys.argv[1]))
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1]))
+    user_data = user.json()
+    tasks_data = tasks.json()
+    total_tasks = 0
+    done_tasks = 0
+    for tasks in tasks_data:
+        if tasks['completed'] is True:
+            done_tasks = done_tasks + 1
+        total_tasks = total_tasks + 1
+    print("Employee {} is done with tasks({}/{}):".
+          format(user_data[0]['name'], done_tasks, total_tasks))
+    for tasks in tasks_data:
+        if tasks['completed'] is True:
+            print("\t {}".format(tasks['title']))

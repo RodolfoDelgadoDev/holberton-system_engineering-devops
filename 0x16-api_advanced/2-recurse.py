b3560ed92aceb003a2b5283817565299acada283
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-28 21:35:05 +0000

    added ex 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,12 +18,16 @@ def rec(l, hot_list, i):
 
 def recurse(subreddit, hot_list=[]):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    r = requests.get(url, headers={'User-Agent': 'Rochos'})
+    r = requests.get(url, headers={'User-Agent': 'Rochos'},
+                     allow_redirects=False)
     if r.status_code == 200:
         di = r.json()
         l = di['data']['children']
         hot_list = []
         i = 0
-        return rec(l, hot_list, i)
+        lista = rec(l, hot_list, i)
+        if len(lista) == 0:
+                return None
+        return lista
     else:
         return None

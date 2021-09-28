01f84f29fc58e335339009d63a3c7f7d56972eed
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-28 22:14:32 +0000

    crazy man

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,25 +7,32 @@ hot articles for a given subreddit.
 import requests
 
 
-def rec(l, hot_list, i):
-        if i == len(l):
-            return hot_list
+def rec(subreddit, url, di, l, hot_list, i):
+        if i == len(l) and di['data']['after']:
+                url = 'https://www.reddit.com/r/{}/hot.json?after={}'\
+                      .format(subreddit, di['data']['after'])
+                r = requests.get(url, headers={'User-Agent': 'Rochos'})
+                di = r.json()
+                l = di['data']['children']
+                i = 0
+                return rec(subreddit, url, di, l, hot_list, i)
+        elif di['data']['after'] is None:
+                return hot_list
         else:
             hot_list.append(l[i]['data']['title'])
             i = i + 1
-            return rec(l, hot_list, i)
+            return rec(subreddit, url, di, l, hot_list, i)
 
 
 def recurse(subreddit, hot_list=[]):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    r = requests.get(url, headers={'User-Agent': 'Rochos'},
-                     allow_redirects=False)
+    r = requests.get(url, headers={'User-Agent': 'Rochos'})
     if r.status_code == 200:
         di = r.json()
         l = di['data']['children']
         hot_list = []
         i = 0
-        lista = rec(l, hot_list, i)
+        lista = rec(subreddit, url, di, l, hot_list, i)
         if len(lista) == 0:
                 return None
         return lista

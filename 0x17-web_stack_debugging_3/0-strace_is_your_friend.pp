e9899ea26069e8d366aceb11b80b3aeadb3db30a
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-29 19:47:39 +0000

    added ex 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+# settings
+exec {'Correct Setting':
+    command => "/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    }
+    exec { 'Restart Apache2':
+    	 require => Exec['Correct Setting'],
+	 command => '/usr/bin/service apache2 restart',
+}
